1b981b2eddacbbea4c3a201f416a50e0bf411d43
Shelvacu <github@shelvacu.com>
2021-06-20 02:27:21 -0700

    WIP: Add newline support

--- dev.bash
+++ dev.bash
@@ -5,12 +5,15 @@ if ! this_dir=$(cd "$(dirname "$0")" && pwd); then
     exit $?
 fi
 
+MCFLY_BIN_PATH=`mktemp -d`
+ln -s $(readlink -f $(find target/debug/deps/mcfly-* -maxdepth 1 -type f | grep -v '\.d')) $MCFLY_BIN_PATH/mcfly
+
 rm -f target/debug/mcfly
 rm -rf target/debug/deps/mcfly-*
 cargo build
 # For some reason, to get line numbers in backtraces, we have to run the binary directly.
 HISTFILE=$HOME/.bash_history \
-  MCFLY_PATH=$(find target/debug/deps/mcfly-* -maxdepth 1 -type f | grep -v '\.d') \
+  PATH="$MCFLY_BIN_PATH:$PATH" \
   RUST_BACKTRACE=full \
   MCFLY_DEBUG=1 \
   exec /bin/bash --init-file "$this_dir/mcfly.bash" -i
